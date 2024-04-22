f4c3a00d289d818b7552a898aaa111149eb0ce2a
ghorwood <ghorwood@macs.vic.edu.au>
2024-04-22 15:17:03 +1000

    Fixed Issue With Add Product

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -15,5 +15,21 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Quantity Must Be Greater Than Or Equal To Zero")]
         public double Price { get; set; }
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(x => x.Inventory != null &&
+                x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                this.ProductInventories.Add(new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = this.ProductId,
+                    Product = this
+                });
+            }
+        }
     }
 }
