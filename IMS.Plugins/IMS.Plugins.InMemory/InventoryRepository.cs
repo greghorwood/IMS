12bbdad82cf9056977f14802986862ee6773b065
ghorwood <ghorwood@macs.vic.edu.au>
2024-05-07 10:47:13 +1000

    Start Of Lesson 43 - Produce Product Use Case

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -15,9 +15,7 @@ namespace IMS.Plugins.InMemory
                 new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
                 new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
                 new Inventory { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8 },
-                new Inventory { InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 20, Price = 1 },
-                new Inventory { InventoryId = 5, InventoryName = "My Bike", Quantity = 20, Price = 3000 },
-                new Inventory { InventoryId = 6, InventoryName = "Greg's Bike", Quantity = 1, Price = 10000 }
+                new Inventory { InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 20, Price = 1 }
             };
         }
 
