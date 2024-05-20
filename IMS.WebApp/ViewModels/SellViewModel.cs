81c9ac5ada4f844471ad13e3ed47e41448c05657
ghorwood <ghorwood@macs.vic.edu.au>
2024-05-20 15:51:08 +1000

    Finished 47_To_50

---
+++ IMS.WebApp/ViewModels/SellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using IMS.CoreBusiness;
+using IMS.WebApp.ViewModelsValidations;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class SellViewModel
+    {
+        [Required]
+        public string SaleOrderNumber { get; set; } = string.Empty;
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than one.")]
+        
+        [Sell_EnsureEnoughProductQuantity]
+        public int QuantityToSell { get; set; }
+
+        [Required]
+        [Range(minimum: 0, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than or equal to zero.")]
+        public double UnitPrice { get; set; }
+        public Product? Product { get; set; } = null;
+    }
+}
