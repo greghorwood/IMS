23db8f23bd9c4ebc7c1122544fb066d60c05694a
ghorwood <ghorwood@macs.vic.edu.au>
2024-05-29 09:43:53 +1000

    Added IMSContext

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -14,5 +14,7 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price Must Be Greater Than Or Equal To Zero")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
     }
 }
---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/IMSContext.cs
@@ -0,0 +1,30 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Plugins.EFCoreSqlServer
+{
+    public class IMSContext : DbContext
+    {
+        public  DbSet<Inventory>? Inventories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<ProductInventory>? ProductInventories { get; set; }
+        public DbSet<InventoryTransaction>? InventoryTransactions { get; set; }
+        public DbSet<ProductTransaction>? ProductTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductInventory>()
+                .HasKey(pi => new { pi.ProductId, pi.InventoryId });
+
+            modelBuilder.Entity<ProductInventory>()
+                .HasOne(pi => pi.Product)
+                .WithMany(p => p.ProductInventories)
+                .HasForeignKey(pi => pi.ProductId);
+
+            modelBuilder.Entity<ProductInventory>()
+                .HasOne(pi => pi.Inventory)
+                .WithMany(i => i.ProductInventories)
+                .HasForeignKey(pi => pi.InventoryId);
+        }
+    }
+}
