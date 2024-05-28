8d64d55e4d5829968dd51b98fd08ceee6a2fd732
ghorwood <ghorwood@macs.vic.edu.au>
2024-05-29 09:53:02 +1000

    Just Before Running DB Migrations

--- IMS.Plugins/IMS.Plugins.EFCoreSqlServer/IMSContext.cs
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/IMSContext.cs
@@ -25,6 +25,26 @@ namespace IMS.Plugins.EFCoreSqlServer
                 .HasOne(pi => pi.Inventory)
                 .WithMany(i => i.ProductInventories)
                 .HasForeignKey(pi => pi.InventoryId);
+
+            //seeding data
+            modelBuilder.Entity<Inventory>().HasData(
+                new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+                new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15 },
+                new Inventory { InventoryId = 3, InventoryName = "Bike Wheel", Quantity = 20, Price = 8 },
+                new Inventory { InventoryId = 4, InventoryName = "Bike Pedal", Quantity = 20, Price = 1 }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { ProductId = 1, ProductName = "Bike", Quantity = 10, Price = 150 },
+                new Product() { ProductId = 2, ProductName = "Car", Quantity = 5, Price = 25000 }
+            );
+
+            modelBuilder.Entity<ProductInventory>().HasData(
+                new ProductInventory { ProductId = 1, InventoryId = 1, InventoryQuantity = 1}, //seat
+                new ProductInventory { ProductId = 1, InventoryId = 2, InventoryQuantity = 1}, //body
+                new ProductInventory { ProductId = 1, InventoryId = 3, InventoryQuantity = 2}, //wheels
+                new ProductInventory { ProductId = 1, InventoryId = 4, InventoryQuantity = 2} //pedals
+            );
         }
     }
 }
